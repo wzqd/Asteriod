4e53ed5bf386a9c78eacf4d3eee1c258efd4ef5b
Jason Gu <1057337859@qq.com>
2022-09-28 09:04:52 -0700

    physics related

--- Assets/Scripts/GameObjects/SpaceShip.cs
+++ Assets/Scripts/GameObjects/SpaceShip.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -29,13 +30,15 @@ public class SpaceShip : MonoBehaviour
     }
     void Update()
     {
-        Move();
         Shoot();
         OutOfScreenDetection();
 
-        
     }
 
+    private void FixedUpdate() //physic related
+    {
+        Move();
+    }
 
 
     private void Move()
